d2363722d901f8c54ca7792c895d854040b8f4b0
BhupeshKumarNaik <154269583+BhupeshKumarNaik@users.noreply.github.com>
2025-02-21 19:07:10 +0530

    Delete day-4-statefile-local-issues/main.tf

--- day-4-statefile-local-issues/main.tf
+++
@@ -1,9 +0,0 @@
-resource "aws_instance" "name" {
-  ami = "ami-0ddfba243cbee3768"
-  instance_type = "t2.micro"
-  key_name = "webserver-key"
-  availability_zone = "ap-south-1a"
-  tags = {
-    name = "naik"
-  }
-}
